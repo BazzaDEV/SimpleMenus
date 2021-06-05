d488354e61cf3f06fe7c859968ada82ea3854613
BazzaDEV <bazarjani.kian@gmail.com>
2021-06-04 21:52:14 -0400

    Added print, println, getInput helper methods

--- src/main/java/utils/TextUtils.java
+++ src/main/java/utils/TextUtils.java
@@ -1,8 +1,45 @@
 package utils;
 
+import java.util.Scanner;
+
 public class TextUtils {
 
     public static final String HEADER_BRACKET_STARS = "[********************************]";
+
+    /**
+     * A helper method to eliminate the redundancy of typing "System.out.println()" every time
+     * a user wishes to print to the console.
+     *
+     * @param s the string to print to the console
+     */
+    public static void println(String s) {
+        System.out.println(s);
+    }
+
+    /**
+     * A helper method to eliminate the redundancy of typing "System.out.print()" every time
+     * a user wishes to print to the console.
+     * @param s the string to print to the console
+     */
+    public static void print(String s) {System.out.print(s);}
+
+    /**
+     * Returns a user's given input from a specified prompt.
+     *
+     * @param prompt a prompt for the user's input
+     * @return the user's provided input
+     */
+    public static String getInput(String prompt) {
+        print(prompt);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine().trim();
+        sc.close();
+
+        return input;
+
+    }
+
 
     public static void printEmptyLines(int n) {
         StringBuilder stringBuilder = new StringBuilder();
